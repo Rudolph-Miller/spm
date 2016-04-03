bb97cc9bf91ef2a82d6b04146c012c852c043a00
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-04-03 23:00:00 +0900

    Handle already installed plugins

--- command/install.go
+++ command/install.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"fmt"
+	"github.com/Rudolph-Miller/spm/util"
 	"github.com/codegangsta/cli"
 	"os"
 	"os/exec"
@@ -17,9 +18,14 @@ func CmdInstall(c *cli.Context, directory string) {
 	repo := args.First()
 	_, name := filepath.Split(repo)
 	target := filepath.Join(directory, name)
-	out, err := exec.Command("git", "clone", repo, target).CombinedOutput()
-	fmt.Println(string(out))
-	if err != nil {
-		os.Exit(1)
+	exists, _ := spm_util.Exists(target)
+	if exists {
+		fmt.Printf("%s is already installed.\n", name)
+	} else {
+		out, err := exec.Command("git", "clone", repo, target).CombinedOutput()
+		fmt.Println(string(out))
+		if err != nil {
+			os.Exit(1)
+		}
 	}
 }
