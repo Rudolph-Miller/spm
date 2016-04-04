c10798a439e5ab96af4fb4fb8742e4ffea73ea55
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-04-04 09:45:51 +0900

    Use Buffer

--- command/export.go
+++ command/export.go
@@ -1,10 +1,40 @@
 package command
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/Rudolph-Miller/spm/struct"
 	"github.com/codegangsta/cli"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 func CmdExport(c *cli.Context, directory string) {
-	fmt.Println(c.String("output"))
+	files, err := ioutil.ReadDir(directory)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var buffer bytes.Buffer
+
+	for _, file := range files {
+		name := file.Name()
+		source := spm_struct.Source{
+			Name: name,
+			Path: filepath.Join(directory, name),
+		}
+		switch source.Type() {
+		case spm_struct.Git:
+			buffer.Write([]byte("git"))
+		case spm_struct.Other:
+			buffer.Write([]byte("other"))
+		}
+		buffer.Write([]byte{' '})
+		buffer.Write([]byte(source.Name))
+		buffer.Write([]byte{'\n'})
+	}
+	fmt.Println(buffer.String())
 }
